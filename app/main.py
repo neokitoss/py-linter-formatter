32dea19ad5a7c8db493ff0e0bc39e59dbafc21fd
Neokitoss <kpokodiji13@gmail.com>
2026-03-22 00:31:19 +0200

    bug fixed in py-linter-formatter solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"}
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
-        [
-            {
-                "line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"
-             }
-            for error in errors
-        ],
-    "path": file_path,
-    "status": "failed" if errors else "passed"
+            [
+                {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                }
+                for error in errors
+            ],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
     }
 
 
@@ -22,13 +32,16 @@ def format_linter_report(linter_report: dict) -> list:
             "errors":
                 [
                     {
-                        "line": error["line_number"], "column": error["column_number"], "message": error["text"],
-                        "name": error["code"], "source": "flake8"
+                        "line": error["line_number"],
+                        "column": error["column_number"],
+                        "message": error["text"],
+                        "name": error["code"],
+                        "source": "flake8"
                     }
                     for error in errors
                 ],
             "path": path,
             "status": "failed" if errors else "passed"
         }
-    for path, errors in linter_report.items()
+        for path, errors in linter_report.items()
     ]
