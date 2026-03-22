bbcf8a0e31965490e6119bacdb2de1e17bd6d2f0
Neokitoss <kpokodiji13@gmail.com>
2026-03-22 18:41:26 +0200

    bug fix

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "status": "failed" if errors else "passed"
     }
 
+
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(path, errors)
             for path, errors in linter_report.items()]
